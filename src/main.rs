a04a89642638c7697f0f5dc977dde88b5c3ab084
ahong <2260071555@qq.com>
2025-02-11 18:00:13 +0800

    实现命令行参数读取功能，并添加文件内容读取逻辑

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use std::env:: args;
+
 mod zfile;
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = args().collect();
+    if args.len() < 2 {
+       println!("Usage: zshrc <file>");
+       return;
+    }
+    let filename= &args[1];
+    let content = zfile::read_zfile(filename);
+    match content {
+         Ok(lines) => {
+              for line in lines {
+                println!("{}", line);
+              }
+         },
+         Err(e) => {
+              println!("Error: {}", e);
+         }
+    }
+   
+    
+
 }
